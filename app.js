32f51bc67ee7c37ae99c641455ee0d40b531410d
thisisstreamliner <thisisstreamliner@gmail.com>
2019-09-30 21:22:16 +0300

    Section 6.84. Finishing the Pug Template

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('page_not_found')
+  res.status(404).render('page_not_found', {pageTitle: 'Page Not Found!'})
   //res.status(404).sendFile(path.join(__dirname, 'views', 'page_not_found.html'))
 })
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   console.log('shop.js',adminData.products)
   const products = adminData.products
-  res.render('shop', {prods: products, docTitle: 'Shop'})
+  res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'})
   //return res.sendFile(path.join(rootDir,'views', 'shop.html'))
 })
 
