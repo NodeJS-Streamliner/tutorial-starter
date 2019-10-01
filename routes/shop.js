b637b1075fe94795531db9a24ce77e3913866de0
thisisstreamliner <thisisstreamliner@gmail.com>
2019-10-01 19:29:24 +0300

    Section 6.87. Converting our Project to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   console.log('shop.js',adminData.products)
   const products = adminData.products
-  res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'})
+  res.render('shop', {prods: products, pageTitle: 'Shop', path:'/', hasProducts: products.length > 0})
   //return res.sendFile(path.join(rootDir,'views', 'shop.html'))
 })
 
