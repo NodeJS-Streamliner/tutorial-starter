4dedbeb368082df1bfc87a21c0a4a20bca0721d6
Tiger <thisisstreamliner@gmail.com>
2019-10-12 18:46:53 +0300

    Section 7.69. Adding Controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,9 @@
 const express = require('express')
 const path = require('path')
-const rootDir = require('../util/path')
-const adminData = require('./admin')
 const router = express.Router()
+const productController = require('../controllers/products')
 
 
-router.get('/', (req, res, next) => {
-  console.log('shop.js',adminData.products)
-  const products = adminData.products
-  res.render('shop', {
-    prods: products,
-     pageTitle: 'Shop',
-     path:'/',
-     hasProducts: products.length > 0,
-     activeShop: true,
-     productCSS: true
-   })
-  //return res.sendFile(path.join(rootDir,'views', 'shop.html'))
-})
+router.get('/', productController.getProducts)
 
 module.exports = router
