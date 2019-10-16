19b805a962798c129d8a025e6a1c4e6932fe4282
Tiger <thisisstreamliner@gmail.com>
2019-10-16 20:47:36 +0300

    Section 7.99 Storing Data In Files Via The Model and 7.100 Fetching Data From Files Via The Model

--- models/product.js
+++ models/product.js
@@ -1,14 +1,33 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
 module.exports = class Product {
   constructor(t) {
     this.title = t
   }
 
   save() {
-    products.push(this)
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      let products = []
+      if (!error) {
+        products = JSON.parse(fileContent)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (e) => {
+        console.log(e)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products 
+  static fetchAll(callback) {
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        callback([])
+      } else {
+        callback(JSON.parse(fileContent))
+      }
+    })
   }
 }
