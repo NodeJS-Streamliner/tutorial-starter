d766249b4630ccf164d0949ab9d7662af0fb0de5
Tiger <thisisstreamliner@gmail.com>
2020-02-19 16:09:48 +0200

    Creating and managing a dummy user (added user in all requests as middle function

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ const User = require('./models/user')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user
+      next()
+    }).catch(e => {
+      console.error(e)
+    })
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
@@ -29,10 +39,23 @@ Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product)
 
 sequelize
-  .sync({force: true}) // create tables for your model when you called define method
+  //.sync({force: true})  // drop table and re-create
+  .sync() // create tables for your model when you called define method
   .then(result => {
     console.log('success connection to database')
-
+    return User.findByPk(1)
+  })
+  .then(user => {
+    if (!user){
+      return User.create({
+        name: 'Max',
+        email: 'test@test.com'
+      })
+    }
+    return Promise.resolve(user);
+  })
+  .then(user => {
+    console.log(user)
     app.listen(3000)
   })
   .catch(e => {
