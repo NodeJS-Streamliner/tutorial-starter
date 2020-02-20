48b5dae314d30b65e54b3e7a0d1e82090f95ea15
Tiger <thisisstreamliner@gmail.com>
2020-02-20 12:48:21 +0200

    Fetching Related Products (for a specific user)

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,8 +39,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const productId = req.params.productId
-  Product.findByPk(productId)
-    .then(product => {
+  req.user
+    .getProducts({where: {id: productId}})
+    //Product.findByPk(productId)
+    .then(products => {
+      const product = products[0]
       if (!product) return res.redirect('/')
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
@@ -99,7 +102,9 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+  //Product.findAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
