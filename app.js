65e1acd21b57d442300133811e79ae89327bf75d
Tiger <thisisstreamliner@gmail.com>
2020-05-04 15:01:31 +0300

    Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -6,72 +6,24 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+//const adminRoutes = require('./routes/admin')
+//const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
-
-const sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
-
+const mongoConnect = require('./util/database')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user
-      next()
-    }).catch(e => {
-      console.error(e)
-    })
+
 })
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+//app.use('/admin', adminRoutes)
+//app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-// if user delete all related products to this user will be deleted too
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
-User.hasMany(Product)
-
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, {through: CartItem})
-Product.belongsToMany(Cart, {through: CartItem})
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, {through:  OrderItem })
-
-sequelize
-  //.sync({force: true})  // drop table and re-create
-   .sync() // create tables for your model when you called define method
-  .then(result => {
-    console.log('success connection to database')
-    return User.findByPk(1)
-  })
-  .then(user => {
-    if (!user){
-      return User.create({
-        name: 'Max',
-        email: 'test@test.com'
-      })
-    }
-    return Promise.resolve(user);
-  })
-  .then(user => {
-    return user.createCart()
-  })
-  .then(cart => {
-    console.log(cart)
-    app.listen(3000)
-  })
-  .catch(e => {
-    console.error(e)
-  })
+mongoConnect((client) => {
+  console.log(client)
+  app.listen(3000)
+})
--- util/database.js
+++ util/database.js
@@ -1,5 +1,13 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', '12345678', {dialect: 'mysql', host : 'localhost'})
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://streamliner:12345678aA@cluster0-zxjuk.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client =>  {
+      console.log('connected')
+      callback(client)
+    })
+    .catch(error => console.error(error))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
