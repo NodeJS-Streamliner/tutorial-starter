d34242443249e8f567cdcc8ff80fd3c3525d81cb
thisisstreamliner <thisisstreamliner@gmail.com>
2020-08-25 15:52:59 +0300

    Displaying the Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,15 +56,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart.getProducts()
-                .then(products => {
-                  res.render('shop/cart', {
-                    path: '/cart',
-                    pageTitle: 'Your Cart',
-                    products: products
-                  })
-                }).catch(error => console.error(error))
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      })
     })
     .catch(err => console.error(err))
   /*Cart.getCart(cart => {
@@ -91,8 +88,9 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product)
   }).then(result => {
     console.log(result)
+    res.redirect('/cart')
   })
-  res.redirect('/cart')
+  
   /*let fetchedCart;
   let newQuantity = 1
   req.user.getCart()
