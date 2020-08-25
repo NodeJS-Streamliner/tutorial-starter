0bf2718b80da0b3e283a15d715feaf15713f650d
thisisstreamliner <thisisstreamliner@gmail.com>
2020-08-25 16:02:47 +0300

    Deleting Cart Item

--- controllers/shop.js
+++ controllers/shop.js
@@ -90,45 +90,13 @@ exports.postCart = (req, res, next) => {
     console.log(result)
     res.redirect('/cart')
   })
-  
-  /*let fetchedCart;
-  let newQuantity = 1
-  req.user.getCart()
-        .then(cart => {
-          fetchedCart = cart
-          return cart.getProducts({where: {id: productId}})
-        }).then(products => {
-          let product;
-          if (products.length > 0) {
-            product = products[0]
-          }
-          if (product) {
-            const oldQuantity = product.cartItem.quantity
-            newQuantity = oldQuantity + 1
-            return product
-          }
-          return Product.findByPk(productId)
-        }).then(product => {
-          return fetchedCart.addProduct(product, {through: {quantity: newQuantity}})
-        }).then(() => {
-          res.redirect('/cart')
-        }).catch(error => console.error(error))
-  /*Product.findById(productId, (product) => {
-    Cart.addProduct(productId, parseFloat(product.price))
-  })
-  res.redirect('/cart')
-  */
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId
-  req.user.getCart()
-    .then(cart =>{
-      return cart.getProducts({where: {id: productId}})
-    }).then(products => {
-      const product = products[0]
-      product.cartItem.destroy()
-    }).then(result => {
+  req.user
+    .deleteItemFromCart(productId)
+    .then(result => {
       res.redirect('/cart')
     }).catch(error => console.error(error))
 }
--- routes/shop.js
+++ routes/shop.js
@@ -18,7 +18,7 @@ router.post('/cart', shopController.postCart)
 
 //router.get('/checkout', shopController.getCheckout)
 
-//router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+router.post('/cart-delete-item', shopController.postCartDeleteProduct)
 
 //router.post('/create-order', shopController.postOrder)
 
