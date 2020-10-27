fdc9ccbf1ad826e8bb86b0c2f8487c3a00c3bf25
thisisstreamliner <thisisstreamliner@gmail.com>
2020-10-27 17:23:32 +0200

    Adding User Model

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
-//const User = require('./models/user')
+const User = require('./models/user')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-/*app.use((req, res, next) => {
-  User.findById("5ee7658d68d4bec681d88be6")
+app.use((req, res, next) => {
+  User.findById("5f9838ec4d747957e4a1aa9b")
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id)
       next()
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
     .catch(error => {
       console.error(error)
     })
-})*/
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -33,6 +33,18 @@ app.use(errorController.get404)
 
 mongoose.connect('mongodb+srv://streamliner:12345678aA@cluster0-zxjuk.mongodb.net/test?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save()
+      }
+    })
     app.listen(3000)
   })
   .catch(err => {
